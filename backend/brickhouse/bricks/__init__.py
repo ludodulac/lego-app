aeb13d2deb427ee9b8fd3e0cd356e4235f503344
ludodulac <162371616+ludodulac@users.noreply.github.com>
2026-08-19 12:12:46 +0200

    BH-005: export wall placement API

--- backend/brickhouse/bricks/__init__.py
+++ backend/brickhouse/bricks/__init__.py
@@ -2,5 +2,13 @@
 
 from brickhouse.bricks.catalog import create_m0_brick_catalog
 from brickhouse.bricks.models import BrickCatalog, BrickDefinition
+from brickhouse.bricks.placement import BrickPlacement, WallBrickLayout, generate_simple_wall_layout
 
-__all__ = ["BrickCatalog", "BrickDefinition", "create_m0_brick_catalog"]
+__all__ = [
+    "BrickCatalog",
+    "BrickDefinition",
+    "BrickPlacement",
+    "WallBrickLayout",
+    "create_m0_brick_catalog",
+    "generate_simple_wall_layout",
+]
