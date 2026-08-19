bf955f91087b7c5aecf5ab015fa1087cca2b7e42
ludodulac <162371616+ludodulac@users.noreply.github.com>
2026-08-19 12:51:05 +0200

    BH-011: export gable roof primitives

--- backend/brickhouse/bricks/__init__.py
+++ backend/brickhouse/bricks/__init__.py
@@ -14,6 +14,14 @@ from brickhouse.bricks.placement import (
     generate_simple_wall_layout,
     generate_wall_layout_with_openings,
 )
+from brickhouse.bricks.roof import (
+    GlobalRoofPlacement,
+    RoofPartCatalog,
+    RoofPartDefinition,
+    SpatialRoof,
+    create_m0_roof_catalog,
+    generate_spatial_gable_roof,
+)
 from brickhouse.bricks.scaling import (
     WallGridSpec,
     discretize_wall_geometry,
@@ -33,16 +41,22 @@ __all__ = [
     "BuildingBrickShell",
     "BuildingWallLayout",
     "GlobalBrickPlacement",
+    "GlobalRoofPlacement",
+    "RoofPartCatalog",
+    "RoofPartDefinition",
     "SpatialBrickShell",
+    "SpatialRoof",
     "WallBrickLayout",
     "WallGridSpec",
     "WallOpeningGrid",
     "create_m0_brick_catalog",
+    "create_m0_roof_catalog",
     "discretize_wall_geometry",
     "discretize_wall_geometry_at_scale",
     "generate_building_brick_shell",
     "generate_scaled_wall_layout",
     "generate_simple_wall_layout",
     "generate_spatial_brick_shell",
+    "generate_spatial_gable_roof",
     "generate_wall_layout_with_openings",
 ]
