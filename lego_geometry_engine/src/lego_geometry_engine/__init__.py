cbc5c86e1e8e6c3a6900df0c325bdb70dc3d3df3
ludodulac <162371616+ludodulac@users.noreply.github.com>
2026-08-31 00:40:31 +0200

    Use spatial broad phase in public assembly API

--- lego_geometry_engine/src/lego_geometry_engine/__init__.py
+++ lego_geometry_engine/src/lego_geometry_engine/__init__.py
@@ -1,8 +1,35 @@
 from .core import (
-    AABB, AssemblyReport, Connector, PartDefinition, PartInstance,
-    Relation, Transform, analyze_assembly, check_collision, find_connections,
-    find_contacts, instantiate, instance_from_dict, transform_from_ldraw,
+    AABB,
+    AssemblyReport,
+    Connector,
+    PartDefinition,
+    PartInstance,
+    Relation,
+    Transform,
+    check_collision,
+    find_connections,
+    find_contacts,
+    instance_from_dict,
+    instantiate,
+    transform_from_ldraw,
 )
 from .library import LDrawLibrary
+from .assembly import analyze_assembly
 
-__all__ = [name for name in globals() if not name.startswith('_')]
+__all__ = [
+    "AABB",
+    "AssemblyReport",
+    "Connector",
+    "LDrawLibrary",
+    "PartDefinition",
+    "PartInstance",
+    "Relation",
+    "Transform",
+    "analyze_assembly",
+    "check_collision",
+    "find_connections",
+    "find_contacts",
+    "instance_from_dict",
+    "instantiate",
+    "transform_from_ldraw",
+]
